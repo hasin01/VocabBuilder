93cf6fb5fac4747fd82eff08a0c2403a5b887f0a
hasin01 <81334215+hasin01@users.noreply.github.com>
2025-12-10 23:11:51 +0200

    export logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { CgMenuRightAlt } from "react-icons/cg";
 import { FaUser } from "react-icons/fa";
+import logo from '../../img/logo.svg';
 
 export const Logo = () => {
 
@@ -9,7 +10,7 @@ export const Logo = () => {
   return (
     <div >
     <a className="flex items-center">
-      <img src="src/img/logo.svg" alt="VocabBuilder Logo" />
+      <img src={logo} alt="VocabBuilder Logo" />
       <p className=" ml-4 text-xl font-medium">VocabBuilder</p>
     </a>
     </div>
